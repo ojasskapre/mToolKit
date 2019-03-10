ff99bc12f19811a819796df2782ce1ffa9d3ee31
ojasskapre <ojas.kapre@somaiya.edu>
2019-03-10 12:44:18 +0530

    My WiFi details collected

---
+++ app/src/main/java/com/example/mtoolkit/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mtoolkit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button mMyWifi;
+    Button mNetworks;
+    Button mSavedWifi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMyWifi = findViewById(R.id.my_wifi);
+        mNetworks = findViewById(R.id.networks);
+        mSavedWifi = findViewById(R.id.saved_wifi);
+
+        mMyWifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(MainActivity.this, MyWifiActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mtoolkit/MyWifiActivity.java
@@ -0,0 +1,131 @@
+package com.example.mtoolkit;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.DhcpInfo;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MyWifiActivity extends AppCompatActivity {
+    WifiManager mWifiManager;
+    WifiInfo mWifiInfo;
+    TextView mMyWifiDetailsText;
+    TextView mMyWifiNameText;
+    TextView mMyWifiDHCPText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_wifi);
+        getMyWifiDetails();
+    }
+
+    @SuppressLint("HardwareIds")
+    private void getMyWifiDetails() {
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        mWifiInfo = mWifiManager.getConnectionInfo();
+
+        mMyWifiNameText = findViewById(R.id.my_wifi_name);
+        mMyWifiNameText.setText(mWifiInfo.getSSID().toUpperCase());
+
+        StringBuilder detail = new StringBuilder();
+        detail.append("\nSSID:\t").append(mWifiInfo.getSSID());
+        detail.append("\nBSSID:\t").append(mWifiInfo.getBSSID().toUpperCase());
+
+        int ip = mWifiInfo.getIpAddress();
+        String result = convertIPformat(ip);
+
+        detail.append("\nIP ADDRESS:\t").append(result);
+        detail.append("\nMAC ADDRESS:\t").append(mWifiInfo.getMacAddress().toUpperCase());
+        detail.append("\nLINK SPEED:\t").append(mWifiInfo.getLinkSpeed());
+        detail.append("\nFREQUENCY:\t").append(mWifiInfo.getFrequency());
+        int level = WifiManager.calculateSignalLevel(mWifiInfo.getRssi(), 100);
+        detail.append("\nSIGNAL STRENGTH:\t").append(level).append("\n");
+
+
+        mMyWifiDetailsText = findViewById(R.id.my_wifi_details);
+        mMyWifiDetailsText.setText(detail);
+
+        DhcpInfo info = mWifiManager.getDhcpInfo();
+
+        StringBuilder dhcp_info = new StringBuilder();
+        dhcp_info.append("\nNETMASK:\t").append(info.netmask);
+
+        ip = info.gateway;
+        result = convertIPformat(ip);
+        dhcp_info.append("\nGATEWAY:\t").append(result);
+
+        ip = info.dns1;
+        result = convertIPformat(ip);
+        dhcp_info.append("\nDNS(1) ADDRESS:\t").append(result);
+
+        ip = info.dns2;
+        result = convertIPformat(ip);
+        dhcp_info.append("\nDNS(2) ADDRESS:\t").append(result);
+
+        ip = info.serverAddress;
+        result = convertIPformat(ip);
+        dhcp_info.append("\nSERVER ADDRESS:\t").append(result);
+        dhcp_info.append("\nLEASE DURATION:\t").append(info.leaseDuration);
+        dhcp_info.append("\nNETWORK ID:\t").append(mWifiInfo.getNetworkId());
+        dhcp_info.append("\nNETWORK TYPE:\t").append(getNetworkClass(getApplicationContext())).append("\n");
+
+        mMyWifiDHCPText = findViewById(R.id.my_wifi_dhcp_details);
+        mMyWifiDHCPText.setText(dhcp_info);
+    }
+
+    private String convertIPformat(int ip) {
+        @SuppressLint("DefaultLocale") String result = String.format("%d.%d.%d.%d", (ip & 0xff), (ip >> 8 & 0xff), (ip >> 16 & 0xff), (ip >> 24 & 0xff));
+        return result;
+    }
+
+    public static String getNetworkClass(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if(info==null || !info.isConnected())
+            return "-"; //not connected
+        if(info.getType() == ConnectivityManager.TYPE_WIFI)
+            return "WIFI";
+        if(info.getType() == ConnectivityManager.TYPE_MOBILE){
+            int networkType = info.getSubtype();
+            switch (networkType) {
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN: //api<8 : replace by 11
+                    return "2G";
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: //api<9 : replace by 14
+                case TelephonyManager.NETWORK_TYPE_EHRPD:  //api<11 : replace by 12
+                case TelephonyManager.NETWORK_TYPE_HSPAP:  //api<13 : replace by 15
+                case TelephonyManager.NETWORK_TYPE_TD_SCDMA:  //api<25 : replace by 17
+                    return "3G";
+                case TelephonyManager.NETWORK_TYPE_LTE:    //api<11 : replace by 13
+                case TelephonyManager.NETWORK_TYPE_IWLAN:  //api<25 : replace by 18
+                case 19:  //LTE_CA
+                    return "4G";
+                default:
+                    return "?";
+            }
+        }
+        return "?";
+    }
+}
